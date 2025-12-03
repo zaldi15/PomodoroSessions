8d09c4d35ab14877fd8d7afc9128a73e3254ba9d
Haidar Yahya <haidaryahya435@gmail.com>
2025-12-03 11:24:30 +0700

    Update MainClass.java

--- src/main/java/com/aplikasi/util/MainClass.java
+++ src/main/java/com/aplikasi/util/MainClass.java
@@ -1,45 +1,47 @@
 package com.aplikasi.util;
 
+import com.aplikasi.view.DashboardController;
+import com.aplikasi.model.User;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import java.io.IOException;
 
 public class MainClass extends Application {
-    
-    // Inisialisasi Statis untuk uji koneksi awal
-    static {
-        // Uji koneksi database saat aplikasi dimuat
-        if (DBConnection.getConnection() == null) {
-            System.err.println("APLIKASI TIDAK DAPAT DILUNCURKAN: Gagal terhubung ke database.");
-            // Di lingkungan nyata, ini harus menampilkan dialog error yang user-friendly
-            System.exit(1); 
-        }
-    }
+
+    private static Stage mainStage;   // Stage utama
 
     @Override
-    public void start(Stage primaryStage) throws IOException {
-        try {
-  
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/aplikasi/view/Register.fxml"));
-            Parent root = loader.load();
-            
-            Scene scene = new Scene(root);
-            primaryStage.setTitle("Login - Aplikasi Pomodoro");
-            primaryStage.setScene(scene);
-            primaryStage.show();
-            
-        } catch (IOException e) {
-            System.err.println("Gagal memuat FXML: " + e.getMessage());
-            e.printStackTrace();
-        }
+    public void start(Stage primaryStage) {
+        mainStage = primaryStage;
+        openLoginPage();
     }
+
+    public static Stage getMainStage() {
+        return mainStage;
+    }
+
+    // ======================
+    // BUKA LOGIN
+    // ======================
+    public static void openLoginPage() {
+        loadScene("/com/aplikasi/view/Login.fxml", "Login - Pomodoro");
+        GloballSession.clearSession();
+    }
+
+    // ======================
+    // BUKA REGISTER
+    // ======================
+    public static void openRegisterPage() {
+        loadScene("/com/aplikasi/view/Register.fxml", "Registrasi");
+    }
+
 
     public static void main(String[] args) {
         launch(args);
         // Pastikan koneksi ditutup saat aplikasi di-close
         DBConnection.closeConnection(); 
     }
+
 }
